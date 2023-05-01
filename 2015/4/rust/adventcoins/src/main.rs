c515012222be25a5ca6f040602767dfbd6ad5cd5
Bruno Lange <blangeram@gmail.com>
2023-04-30 19:41:57 -0700

    Accepts input from stdin

--- 2015/4/rust/adventcoins/src/main.rs
+++ 2015/4/rust/adventcoins/src/main.rs
@@ -1,4 +1,5 @@
 use md5;
+use std::io;
 
 fn nonce(msg: &str) -> Option<u64> {
     (1u64..u64::MAX)
@@ -11,6 +12,14 @@ fn nonce(msg: &str) -> Option<u64> {
 }
 
 fn main() {
-    let value = nonce("bgvyzdsv");
-    println!("value = {:?}", value);
+    let mut input = String::new();
+    while let Ok(_) = io::stdin().read_line(&mut input) {
+        let msg = input.trim();
+        if msg.is_empty() {
+            break;
+        }
+        let value = nonce(msg).unwrap();
+        println!("value = {:?}", value);
+        input.clear();
+    }
 }
