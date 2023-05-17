0ba50b4ff3d440d1ff589f385b6184342c1102ec
Bruno Lange <blangeram@gmail.com>
2023-05-16 21:44:22 -0700

    Parameterized parser

--- 2015/6/rust/lightgrid/src/main.rs
+++ 2015/6/rust/lightgrid/src/main.rs
@@ -1,4 +1,4 @@
-use nom::IResult;
+use nom::{IResult, Parser};
 
 #[allow(unused)]
 fn parse_hello(input: &str) -> IResult<&str, &str, ()> {
@@ -8,9 +8,18 @@ fn parse_hello(input: &str) -> IResult<&str, &str, ()> {
     }
 }
 
+#[allow(unused)]
+fn parse_tag<'i: 't, 't>(tag: &'t str) -> impl Parser<&'i str, &'i str, ()> + 't {
+    move |input: &'i str| match input.strip_prefix(tag) {
+        Some(tail) => Ok((tail, &input[..tag.len()])),
+        None => Err(nom::Err::Error(())),
+    }
+}
+
 #[test]
 fn test() {
     assert_eq!(parse_hello("Hello, World!").unwrap(), (", World!", "Hello"));
+    assert_eq!(parse_tag("Hello").parse("Hello, World!").unwrap(), (", World!", "Hello"));
 }
 
 fn main() {
