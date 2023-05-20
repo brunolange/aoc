f868cb1992f14789ea90e5a3e2c6f1ae447b7fde
Bruno Lange <blangeram@gmail.com>
2023-05-20 10:58:02 -0700

    Renames Coords to GridPoint

--- 2015/6/rust/lightgrid/src/lib.rs
+++ 2015/6/rust/lightgrid/src/lib.rs
@@ -10,51 +10,52 @@ use nom::IResult;
 mod parsers;
 use parsers::{parse_usize, take_word};
 
+/// Stores x and y coordinates of a grid that extends only to the first quadrant.
 #[derive(Debug, PartialEq)]
-pub struct Coords(pub usize, pub usize);
+pub struct GridPoint(pub usize, pub usize);
 
 #[derive(Debug)]
-pub struct ParseCoordsError(String);
+pub struct ParseGridPointError(String);
 
-impl FromStr for Coords {
-    type Err = ParseCoordsError;
+impl FromStr for GridPoint {
+    type Err = ParseGridPointError;
 
     /// Parses the input into a point in the first quadrant of a cartesian grid.
     ///
     /// # Example
     /// ```rust
-    /// use lightgrid::Coords;
+    /// use lightgrid::GridPoint;
     ///
-    /// assert_eq!("100,101".parse::<Coords>().unwrap(), Coords(100, 101));
-    /// assert_eq!("999999,0".parse::<Coords>().unwrap(), Coords(999999, 0));
-    /// assert!("1,2,3".parse::<Coords>().is_err());
-    /// assert!("-1,1".parse::<Coords>().is_err());
-    /// assert!("2,-10".parse::<Coords>().is_err());
+    /// assert_eq!("100,101".parse::<GridPoint>().unwrap(), GridPoint(100, 101));
+    /// assert_eq!("999999,0".parse::<GridPoint>().unwrap(), GridPoint(999999, 0));
+    /// assert!("1,2,3".parse::<GridPoint>().is_err());
+    /// assert!("-1,1".parse::<GridPoint>().is_err());
+    /// assert!("2,-10".parse::<GridPoint>().is_err());
     /// ```
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut parser = all_consuming(separated_pair(parse_usize, char(','), parse_usize));
 
-        let (_, (x, y)) =
-            parser(s).map_err(|_| ParseCoordsError("Unable to parse coordinates".to_string()))?;
+        let (_, (x, y)) = parser(s)
+            .map_err(|_| ParseGridPointError("Unable to parse coordinates".to_string()))?;
 
-        Ok(Coords(x, y))
+        Ok(GridPoint(x, y))
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Rect {
-    pub bottom_left_corner: Coords,
-    pub top_right_corner: Coords,
+    pub bottom_left_corner: GridPoint,
+    pub top_right_corner: GridPoint,
 }
 
 impl Rect {
-    pub fn new(p: Coords, q: Coords) -> Self {
+    pub fn new(p: GridPoint, q: GridPoint) -> Self {
         let (x0, y0) = (p.0, p.1);
         let (x1, y1) = (q.0, q.1);
 
         Rect {
-            bottom_left_corner: Coords(x0.min(x1), y0.min(y1)),
-            top_right_corner: Coords(x0.max(x1), y0.max(y1)),
+            bottom_left_corner: GridPoint(x0.min(x1), y0.min(y1)),
+            top_right_corner: GridPoint(x0.max(x1), y0.max(y1)),
         }
     }
 }
@@ -74,9 +75,9 @@ impl FromStr for Op {
         let (_, (action, _, from, _, to)) = all_consuming(tuple((
             alt((tag("toggle"), tag("turn on"), tag("turn off"))),
             char(' '),
-            parse_coords,
+            parse_grid_point,
             tag(" through "),
-            parse_coords,
+            parse_grid_point,
         )))(s)
         .map_err(|_| ParseOpError("unable to parse line".to_string()))?;
         let rect = Rect::new(from, to);
@@ -89,7 +90,7 @@ impl FromStr for Op {
     }
 }
 
-pub fn parse_coords(input: &str) -> IResult<&str, Coords> {
+pub fn parse_grid_point(input: &str) -> IResult<&str, GridPoint> {
     // map(take_word, |s| s.parse())(input) // this doesn't work, I think because of Err mismatches
     map_res(take_word, |s| s.parse())(input)
 }
@@ -101,32 +102,32 @@ mod tests {
     #[test]
     fn test_rect() {
         assert_eq!(
-            Rect::new(Coords(10, 10), Coords(11, 11)),
+            Rect::new(GridPoint(10, 10), GridPoint(11, 11)),
             Rect {
-                bottom_left_corner: Coords(10, 10),
-                top_right_corner: Coords(11, 11),
+                bottom_left_corner: GridPoint(10, 10),
+                top_right_corner: GridPoint(11, 11),
             }
         );
         assert_eq!(
-            Rect::new(Coords(11, 11), Coords(10, 10)),
+            Rect::new(GridPoint(11, 11), GridPoint(10, 10)),
             Rect {
-                bottom_left_corner: Coords(10, 10),
-                top_right_corner: Coords(11, 11),
+                bottom_left_corner: GridPoint(10, 10),
+                top_right_corner: GridPoint(11, 11),
             }
         );
 
         assert_eq!(
-            Rect::new(Coords(10, 10), Coords(11, 9)),
+            Rect::new(GridPoint(10, 10), GridPoint(11, 9)),
             Rect {
-                bottom_left_corner: Coords(10, 9),
-                top_right_corner: Coords(11, 10),
+                bottom_left_corner: GridPoint(10, 9),
+                top_right_corner: GridPoint(11, 10),
             }
         );
         assert_eq!(
-            Rect::new(Coords(11, 9), Coords(10, 10)),
+            Rect::new(GridPoint(11, 9), GridPoint(10, 10)),
             Rect {
-                bottom_left_corner: Coords(10, 9),
-                top_right_corner: Coords(11, 10),
+                bottom_left_corner: GridPoint(10, 9),
+                top_right_corner: GridPoint(11, 10),
             }
         );
     }
@@ -136,8 +137,8 @@ mod tests {
         assert_eq!(
             "toggle 1,2 through 3,4".parse::<Op>().unwrap(),
             Op::Toggle(Rect {
-                bottom_left_corner: Coords(1, 2),
-                top_right_corner: Coords(3, 4),
+                bottom_left_corner: GridPoint(1, 2),
+                top_right_corner: GridPoint(3, 4),
             })
         );
 
@@ -154,8 +155,8 @@ mod tests {
             Op::Turn(
                 true,
                 Rect {
-                    bottom_left_corner: Coords(1, 2),
-                    top_right_corner: Coords(3, 4),
+                    bottom_left_corner: GridPoint(1, 2),
+                    top_right_corner: GridPoint(3, 4),
                 }
             )
         );
@@ -173,8 +174,8 @@ mod tests {
             Op::Turn(
                 false,
                 Rect {
-                    bottom_left_corner: Coords(1, 2),
-                    top_right_corner: Coords(3, 4),
+                    bottom_left_corner: GridPoint(1, 2),
+                    top_right_corner: GridPoint(3, 4),
                 }
             )
         );
--- 2015/6/rust/lightgrid/src/main.rs
+++ 2015/6/rust/lightgrid/src/main.rs
@@ -1,3 +1,7 @@
+use lightgrid::GridPoint;
+
 fn main() {
     println!("hello, nom!");
+    let point = "100,101".parse::<GridPoint>();
+    println!("here is a point: {:?}", point);
 }
