acc808facf8f55b81f78aa4810405fae63521627
Bruno Lange <blangeram@gmail.com>
2023-05-24 22:37:40 -0700

    Replaces line comment characters

--- 2015/7/rust/circuits/src/io.rs
+++ 2015/7/rust/circuits/src/io.rs
@@ -9,7 +9,7 @@ pub fn lines() -> Box<dyn Iterator<Item = String>> {
                 .lock()
                 .lines()
                 .filter_map(Result::ok)
-                .filter(|line| !line.starts_with("#")),
+                .filter(|line| !line.starts_with("--")),
         ),
         Some(path) => {
             let file = File::open(path).expect("error reading file");
