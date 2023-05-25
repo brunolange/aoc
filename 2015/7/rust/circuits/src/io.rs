760223bc61ae303d5ebd358df16127f6944e336f
Bruno Lange <blangeram@gmail.com>
2023-05-24 21:25:37 -0700

    Submits solution for 2015/7 part 1

--- 2015/7/rust/circuits/src/io.rs
+++ 2015/7/rust/circuits/src/io.rs
@@ -1,3 +1,4 @@
+use std::env::VarError;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -9,4 +10,8 @@ pub fn lines() -> Box<dyn Iterator<Item = String>> {
             Box::new(io::BufReader::new(file).lines().filter_map(Result::ok))
         }
     }
+}
+
+pub fn wire() -> Result<String, VarError> {
+    std::env::var("WIRE")
 }
--- 2015/7/rust/circuits/src/main.rs
+++ 2015/7/rust/circuits/src/main.rs
@@ -4,5 +4,9 @@ mod io;
 
 fn main() {
     let wire_map = run(io::lines());
-    println!("{:?}", wire_map);
+    let wire = io::wire();
+    match wire {
+        Ok(w) => println!("{:?}", wire_map.unwrap().get(&w)),
+        _ => println!("{:?}", wire_map),
+    }
 }
