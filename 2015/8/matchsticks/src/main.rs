00cb7e38c0d2c016ffa9538fa19993e63ce4970c
Bruno Lange <blangeram@gmail.com>
2023-05-25 22:47:55 -0700

    Basic scaffolding

--- 2015/8/matchsticks/src/main.rs
+++ 2015/8/matchsticks/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[allow(unused)]
+fn counts(line: &str) -> (usize, usize) {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_counts() {
+        assert_eq!(counts(""), (2, 0));
+    }
+}
