4456bd4828ae628d60063878296a14019b6c0e14
Bruno Lange <blangeram@gmail.com>
2023-05-26 00:13:38 -0700

    Implements solution for 2015/8 part 1

--- 2015/8/matchsticks/src/main.rs
+++ 2015/8/matchsticks/src/main.rs
@@ -1,10 +1,51 @@
+use nom::branch::alt;
+use nom::bytes::complete::{tag, take_while_m_n};
+use nom::character::complete::alphanumeric1;
+use nom::combinator::all_consuming;
+use nom::multi::many0;
+use nom::sequence::preceded;
+use nom::IResult;
+
+mod io;
+
+use io::lines;
+
 fn main() {
-    println!("Hello, world!");
+    for line in lines() {
+        println!("{}: {:?}", line, counts(&line));
+    }
 }
 
-#[allow(unused)]
+fn is_hex_digit(c: char) -> bool {
+    c.is_digit(16)
+}
+
+fn parse_hex(input: &str) -> IResult<&str, usize> {
+    let (remaining, _) = preceded(tag(r"\x"), take_while_m_n(2, 2, is_hex_digit))(input)?;
+    Ok((remaining, 1))
+}
+
+fn parse_quote(input: &str) -> IResult<&str, usize> {
+    let (remaining, _) = tag("\\\"")(input)?;
+    Ok((remaining, 1))
+}
+
+fn parse_seq(input: &str) -> IResult<&str, usize> {
+    let (remaining, seq) = alphanumeric1(input)?;
+    Ok((remaining, seq.len()))
+}
+
+fn parse_count(input: &str) -> IResult<&str, usize> {
+    let (input, _) = tag("\"")(input)?;
+    let (input, xs) = many0(alt((parse_hex, parse_quote, parse_seq)))(input)?;
+    let _ = all_consuming(tag("\""))(input)?;
+
+    Ok((input, xs.iter().sum()))
+}
+
 fn counts(line: &str) -> (usize, usize) {
-    todo!()
+    let (_, count) = parse_count(line).unwrap();
+    (line.len(), count)
 }
 
 #[cfg(test)]
@@ -13,6 +54,9 @@ mod tests {
 
     #[test]
     fn test_counts() {
-        assert_eq!(counts(""), (2, 0));
+        assert_eq!(counts("\"\""), (2, 0));
+        assert_eq!(counts("\"abc\""), (5, 3));
+        assert_eq!(counts("\"aaa\\\"aaa\""), (10, 7));
+        assert_eq!(counts("\"\\x27\""), (6, 1));
     }
 }
