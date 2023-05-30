647a2ff8448ed69cf38d339ec9293b0be6000ffa
Bruno Lange <blangeram@gmail.com>
2023-05-29 22:29:19 -0700

    Brute force all hamiltonian paths, compares them

--- 2015/9/rust/travelling-santaman/src/main.rs
+++ 2015/9/rust/travelling-santaman/src/main.rs
@@ -1,5 +1,6 @@
 mod io;
 use crate::io::lines;
+use itertools::Itertools;
 use std::collections::HashMap;
 
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -74,5 +75,46 @@ fn main() {
         graph.add_edge(destination, start, edge);
     }
 
-    println!("graph = {:?}", graph);
+    let nodes = graph.adj.keys();
+    let n = nodes.len();
+
+    let hamiltonian_paths = nodes.permutations(n).filter(|permutation| {
+        permutation
+            .iter()
+            .tuple_windows()
+            .all(|(source, destination)| {
+                let neighbors = graph.adj.get(**source).unwrap();
+                neighbors.contains_key(**destination)
+            })
+    });
+
+    let mut min_total_distance = usize::MAX;
+    let mut optimal_path = vec![];
+    for hamiltonian_path in hamiltonian_paths {
+        let total_distance =
+            hamiltonian_path
+                .iter()
+                .tuple_windows()
+                .fold(0, |acc, (source, destination)| {
+                    acc + graph
+                        .adj
+                        .get(**source)
+                        .unwrap()
+                        .get(**destination)
+                        .unwrap()
+                        .distance
+                });
+        println!(
+            "Hamiltonian path: {:?} has total distance: {}",
+            hamiltonian_path, total_distance
+        );
+        if total_distance < min_total_distance {
+            optimal_path = hamiltonian_path;
+            min_total_distance = total_distance;
+        }
+    }
+
+    // println!("graph = {:?}", graph);
+    println!("optimal_path = {:?}", optimal_path);
+    println!("min_total_distance = {:?}", min_total_distance);
 }
