41e2b2b1da3873648b851ac38f2fe02021e15908
Bruno Lange <blangeram@gmail.com>
2023-05-30 20:35:30 -0700

    Sets up an infinite iterator for look and say

--- 2015/10/rust/look-and-say/src/main.rs
+++ 2015/10/rust/look-and-say/src/main.rs
@@ -1,3 +1,36 @@
+struct LookAndSay<'a> {
+    seed: &'a str,
+}
+
+impl LookAndSay<'_> {
+    fn iter(&mut self) -> LookAndSayIter {
+        LookAndSayIter { curr: self.seed }
+    }
+}
+
+struct LookAndSayIter<'a> {
+    curr: &'a str,
+}
+
+impl<'a> Iterator for LookAndSayIter<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let response = Some(self.curr);
+        self.curr = "and then";
+        response
+    }
+}
+
 fn main() {
+    let mut i = 0;
+    let mut las = LookAndSay { seed: "hello" };
+    for s in las.iter() {
+        println!("s = {}", s);
+        i += 1;
+        if i == 5 {
+            break;
+        }
+    }
     println!("Hello, world!");
 }
