3d5c315fb9226708835b786b2b6464e75c96ad4e
Bruno Lange <blangeram@gmail.com>
2023-06-02 23:41:06 -0700

    Ironclad password types

--- 2015/11/rust/next-pwd/src/main.rs
+++ 2015/11/rust/next-pwd/src/main.rs
@@ -1,10 +1,31 @@
-#[derive(Debug)]
-struct PasswordIterator {
-    pwd: [char; 8],
+use nom::{bytes::complete::take_while_m_n, combinator::all_consuming, IResult};
+
+fn parse_pwd<const N: usize>(input: &str) -> IResult<&str, &str> {
+    let (remaining, pwd) = all_consuming(take_while_m_n(N, N, |c: char| c.is_lowercase()))(input)?;
+    Ok((remaining, pwd))
 }
 
-impl Iterator for PasswordIterator {
-    type Item = [char; 8];
+#[derive(Clone, Debug)]
+struct Password<const N: usize> {
+    value: [char; N],
+}
+
+impl<const N: usize> Password<N> {
+    fn from_str(s: &str) -> Option<Self> {
+        let (_, pwd) = parse_pwd::<N>(s).ok()?;
+        let chars: Vec<_> = pwd.chars().collect();
+        let pwd = chars.try_into().ok()?;
+        Some(Password { value: pwd })
+    }
+}
+
+#[derive(Debug)]
+struct PasswordIterator<const N: usize> {
+    pwd: Password<N>,
+}
+
+impl<const N: usize> Iterator for PasswordIterator<N> {
+    type Item = Password<N>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut nxt = self.pwd.clone();
@@ -13,15 +34,17 @@ impl Iterator for PasswordIterator {
         let mut i: i32 = 7;
         while carry && i >= 0 {
             let idx = i as usize;
-            (nxt[idx], carry) = inc(nxt[idx]);
+            (nxt.value[idx], carry) = inc(nxt.value[idx]);
             i -= 1;
         }
 
-        let n = if carry && i == -1 { None } else { Some(nxt) };
+        self.pwd = nxt.clone();
 
-        self.pwd = nxt;
-
-        n
+        if carry && i == -1 {
+            None
+        } else {
+            Some(nxt)
+        }
     }
 }
 
@@ -36,7 +59,7 @@ fn inc(c: char) -> (char, bool) {
 }
 
 fn main() {
-    let pwd = ['h', 'x', 'b', 'x', 'w', 'x', 'b', 'a'];
+    let pwd: Password<8> = Password::from_str("hxbxwxba").expect("invalid password");
     let pi = PasswordIterator { pwd };
     let mut counter = 0;
     for n in pi {
