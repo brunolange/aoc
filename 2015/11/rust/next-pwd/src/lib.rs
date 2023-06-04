146f036bcd91c965ceec8d82071d72b04d762c73
Bruno Lange <blangeram@gmail.com>
2023-06-03 18:14:23 -0700

    Clean up main

--- 2015/11/rust/next-pwd/src/lib.rs
+++ 2015/11/rust/next-pwd/src/lib.rs
@@ -1,4 +1,45 @@
+use std::collections::HashSet;
+
 use nom::{bytes::complete::take_while_m_n, combinator::all_consuming, IResult};
+
+const BLACKLIST: [char; 3] = ['i', 'o', 'l'];
+
+pub fn is_valid_password<const N: usize>(pwd: &Password<N>) -> bool {
+    return password_does_not_contain_blacklisted_characters(pwd)
+        && password_contains_3_characters_in_sequence(pwd)
+        && password_contains_at_least_2_different_pairs_of_letters(pwd);
+}
+
+fn password_does_not_contain_blacklisted_characters<const N: usize>(pwd: &Password<N>) -> bool {
+    let chars = pwd.value;
+    return !BLACKLIST.iter().any(|c| chars.contains(c));
+}
+
+fn password_contains_at_least_2_different_pairs_of_letters<const N: usize>(
+    pwd: &Password<N>,
+) -> bool {
+    let chars = pwd.value;
+    return chars
+        .windows(2)
+        .fold(HashSet::new(), |mut acc, curr| {
+            if curr[0] == curr[1] {
+                acc.insert(&curr[0]);
+            }
+            acc
+        })
+        .len()
+        >= 2;
+}
+
+fn password_contains_3_characters_in_sequence<const N: usize>(pwd: &Password<N>) -> bool {
+    let chars = pwd.value;
+    return chars.windows(3).any(|window| {
+        let left = window[0] as i32;
+        let middle = window[1] as i32;
+        let right = window[2] as i32;
+        return right - middle == middle - left && right - left == 2;
+    });
+}
 
 pub fn parse_pwd<const N: usize>(input: &str) -> IResult<&str, &str> {
     let (remaining, pwd) = all_consuming(take_while_m_n(N, N, |c: char| c.is_lowercase()))(input)?;
--- 2015/11/rust/next-pwd/src/main.rs
+++ 2015/11/rust/next-pwd/src/main.rs
@@ -1,6 +1,4 @@
-use std::collections::HashSet;
-
-use next_pwd::{Password, PasswordIterator};
+use next_pwd::{is_valid_password, Password, PasswordIterator};
 
 mod io;
 
@@ -10,37 +8,7 @@ fn main() {
 
     let mut pi = PasswordIterator { pwd };
 
-    let blacklist = HashSet::from(['i', 'o', 'l']);
-    let next = pi.find(|p| {
-        let chars = p.value;
-        if blacklist.iter().any(|c| chars.contains(c)) {
-            return false;
-        }
-        if chars
-            .windows(2)
-            .fold(HashSet::new(), |mut acc, curr| {
-                if curr[0] == curr[1] {
-                    acc.insert(&curr[0]);
-                }
-                acc
-            })
-            .len()
-            < 2
-        {
-            return false;
-        }
-
-        for window in chars.windows(3) {
-            let left = window[0] as i32;
-            let middle = window[1] as i32;
-            let right = window[2] as i32;
-            if right - middle == middle - left && right - left == 2 {
-                return true;
-            }
-        }
-
-        return false;
-    });
+    let next = pi.find(is_valid_password);
 
     let next_password = next.expect("could not find next password");
     println!("{}", next_password.value.iter().collect::<String>());
