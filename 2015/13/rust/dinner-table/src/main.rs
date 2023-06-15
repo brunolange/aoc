f66c3cfc6ec5cac549f4f00070a20293477ca5b7
Bruno Lange <blangeram@gmail.com>
2023-06-14 23:09:42 -0700

    Builds adjacency list representation of table

--- 2015/13/rust/dinner-table/src/main.rs
+++ 2015/13/rust/dinner-table/src/main.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
+
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, space1};
 use nom::combinator::{map, map_res};
-use nom::sequence::tuple;
 use nom::{
     character::complete::{alpha1, space0},
     sequence::{preceded, terminated},
@@ -45,11 +46,21 @@ fn parse_pairing(input: &str) -> IResult<&str, Pairing> {
 }
 
 fn main() {
-    let s = "Alice would lose 79 happiness units by sitting next to Carol.";
-    let x = parse_pairing(s);
-    println!("x = {:?}", x);
+    let table = vec![
+        "Alice would lose 79 happiness units by sitting next to Carol.",
+        "Alice would gain 54 happiness units by sitting next to Bob.",
+    ]
+    .into_iter()
+    .map(|s| parse_pairing(s).unwrap())
+    .map(|(_, pairing)| pairing)
+    .fold(HashMap::new(), |mut acc, curr| {
+        println!("curr = {:?}", curr);
+        acc.entry(curr.first).or_insert(HashMap::new());
+        acc.get_mut(curr.first)
+            .unwrap()
+            .insert(curr.second, curr.gain);
+        acc
+    });
 
-    let s = "Alice would gain 54 happiness units by sitting next to Bob.";
-    let x = parse_pairing(s);
-    println!("x = {:?}", x);
+    println!("table = {:?}", table);
 }
