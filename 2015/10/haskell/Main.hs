fa676ee4fadd3a2497689160f5264aa2640696ab
Bruno Lange <blangeram@gmail.com>
2023-06-15 21:01:07 -0700

    Renames function

--- 2015/10/haskell/Main.hs
+++ 2015/10/haskell/Main.hs
@@ -1,9 +1,9 @@
 import Data.List
 
-lookAndSays :: String -> String
-lookAndSays xs = intercalate "" $ map f $ groupBy (==) xs
+lookAndSay :: String -> String
+lookAndSay xs = intercalate "" $ map f $ groupBy (==) xs
   where f grp = show (length grp) ++ [head grp]
 
 main :: IO()
 main = print $ (line, length line)
-  where line = last $ take 41 $ iterate lookAndSays "1113122113"
+  where line = last $ take 41 $ iterate lookAndSay "1113122113"
