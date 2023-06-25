a9551e80ff12bf3857a0da4656258b7b6bab1a73
Bruno Lange <blangeram@gmail.com>
2023-06-25 13:04:50 -0700

    Exits with error code if not a single viable next password can be found

--- 2015/11/rust/next-pwd/src/main.rs
+++ 2015/11/rust/next-pwd/src/main.rs
@@ -5,7 +5,14 @@ mod io;
 fn main() {
     env_logger::init();
 
+    let mut found = false;
     for next_pwd in next_passwords(&io::read_password(), io::number_of_passwords()) {
+        found = true;
         println!("{}", next_pwd);
+    }
+
+    if !found {
+        eprintln!("Error: There is no next viable password");
+        std::process::exit(1);
     }
 }
