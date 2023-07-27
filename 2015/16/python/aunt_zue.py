5e965eb7cce17417adea8a08915acdd984592301
Bruno Lange <blangeram@gmail.com>
2023-07-26 22:20:08 -0700

    Minor reorg

--- 2015/16/python/aunt_zue.py
+++ 2015/16/python/aunt_zue.py
@@ -21,6 +21,76 @@ class AuntSue:
     perfumes: int | None
 
 
+@dataclass
+class TickerTape:
+    children: int
+    cats: int
+    samoyeds: int
+    pomeranians: int
+    akitas: int
+    vizslas: int
+    goldfish: int
+    trees: int
+    cars: int
+    perfumes: int
+
+
+def check(aunt_sue: AuntSue, ticker: TickerTape) -> bool:
+    solver = z3.Solver()
+
+    for i, (value, op, reading) in enumerate(
+        [
+            (aunt_sue.children, eq, ticker.children),
+            (aunt_sue.cats, gt, ticker.cats),
+            (aunt_sue.samoyeds, eq, ticker.samoyeds),
+            (aunt_sue.pomeranians, lt, ticker.pomeranians),
+            (aunt_sue.akitas, eq, ticker.akitas),
+            (aunt_sue.vizslas, eq, ticker.vizslas),
+            (aunt_sue.goldfish, lt, ticker.goldfish),
+            (aunt_sue.trees, gt, ticker.trees),
+            (aunt_sue.cars, eq, ticker.cars),
+            (aunt_sue.perfumes, eq, ticker.perfumes),
+        ]
+    ):
+        literal = z3.Int(f"x_{i}")
+        solver.add(op(literal, reading))
+
+        if value is None:
+            continue
+        solver.add(literal == value)
+
+    return solver.check() == z3.sat
+
+
+def main() -> int:
+    match: AuntSue | None = None
+
+    ticker = TickerTape(
+        children=3,
+        cats=7,
+        samoyeds=2,
+        pomeranians=3,
+        akitas=0,
+        vizslas=0,
+        goldfish=5,
+        trees=3,
+        cars=2,
+        perfumes=1,
+    )
+
+    for aunt_sue in parse_line("input"):
+        if check(aunt_sue, ticker):
+            match = aunt_sue
+            break
+
+    if not match:
+        print(f"Found no Aunt Sue...", file=sys.stderr)
+        return 1
+
+    print(f"Found Aunt Sue: {match}")
+    return 0
+
+
 def parse_line(filename) -> Iterator[AuntSue]:
     with open(filename) as handle:
         for line in handle.readlines():
@@ -42,71 +112,6 @@ def parse_line(filename) -> Iterator[AuntSue]:
                 cars=attrs.get("cars"),
                 perfumes=attrs.get("perfumes"),
             )
-
-@dataclass
-class TickerTape:
-    children: int
-    cats: int
-    samoyeds: int
-    pomeranians: int
-    akitas: int
-    vizslas: int
-    goldfish: int
-    trees: int
-    cars: int
-    perfumes: int
-
-def main() -> int:
-    match: AuntSue | None = None
-
-    ticker = TickerTape(
-        children=3,
-        cats=7,
-        samoyeds=2,
-        pomeranians=3,
-        akitas=0,
-        vizslas=0,
-        goldfish=5,
-        trees=3,
-        cars=2,
-        perfumes=1, 
-    )
-
-    for aunt_sue in parse_line("input"):
-        solver = z3.Solver()
-
-        for i, (value, op, reading) in enumerate(
-            [
-                (aunt_sue.children, eq, ticker.children),
-                (aunt_sue.cats, gt, ticker.cats),
-                (aunt_sue.samoyeds, eq, ticker.samoyeds),
-                (aunt_sue.pomeranians, lt, ticker.pomeranians),
-                (aunt_sue.akitas, eq, ticker.akitas),
-                (aunt_sue.vizslas, eq, ticker.vizslas),
-                (aunt_sue.goldfish, lt, ticker.goldfish),
-                (aunt_sue.trees, gt, ticker.trees),
-                (aunt_sue.cars, eq, ticker.cars),
-                (aunt_sue.perfumes, eq, ticker.perfumes),
-            ]
-        ):
-            literal = z3.Int(f"x_{i}")
-            solver.add(op(literal, reading))
-
-            if value is None:
-                continue
-            solver.add(literal == value)
-
-        if solver.check() == z3.sat:
-            match = aunt_sue
-            break
-
-    if not match:
-        print(f"Found no Aunt Sue...", file=sys.stderr)
-        return 1
-
-    print(f"Found Aunt Sue: {match}")
-
-    return 0
 
 
 if __name__ == "__main__":
