e82e5cc1acb9d62010eaed945c7b2602e5883fd5
Bruno Lange <blangeram@gmail.com>
2023-08-19 16:15:33 -0700

    Adds molecule parser

--- 2015/19/medicine/src/main.rs
+++ 2015/19/medicine/src/main.rs
@@ -1,7 +1,30 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
+
+type Atom = String;
+
+#[derive(Debug)]
+struct Molecule {
+    atoms: Vec<Atom>,
+}
+
+impl FromStr for Molecule {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Molecule {
+            atoms: split_on_uppercase(s)
+                .into_iter()
+                .map(|s| s.to_owned())
+                .collect(),
+        })
+    }
+}
 
 fn main() {
-    let molecule = "HOH";
+    let molecule: Molecule = "HCaOCaliforniA".parse().unwrap();
+    // let molecule = "HOH";
     let replacements = [("H", "HO"), ("H", "OH"), ("O", "HH")];
     let replacement_map: HashMap<&str, HashSet<&str>> =
         replacements
@@ -14,6 +37,26 @@ fn main() {
                 acc
             });
 
-    println!("molecule = {}", molecule);
+    println!("molecule = {:?}", molecule.atoms);
     println!("replacement_map = {:?}", replacement_map);
+}
+
+fn split_on_uppercase(s: &str) -> Vec<&str> {
+    let mut result = Vec::new();
+    let mut start = 0;
+
+    for (i, c) in s.char_indices() {
+        if c.is_ascii_uppercase() {
+            if start < i {
+                result.push(&s[start..i]);
+            }
+            start = i;
+        }
+    }
+
+    if start < s.len() {
+        result.push(&s[start..]);
+    }
+
+    result
 }
