c25988633dc9826f9d98ad977962742d81ecb5db
Bruno Lange <blangeram@gmail.com>
2023-08-19 15:25:02 -0700

    Builds replacement map

--- 2015/19/medicine/src/main.rs
+++ 2015/19/medicine/src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::{HashMap, HashSet};
+
 fn main() {
-    println!("Hello, world!")
+    let molecule = "HOH";
+    let replacements = [("H", "HO"), ("H", "OH"), ("O", "HH")];
+    let replacement_map: HashMap<&str, HashSet<&str>> =
+        replacements
+            .into_iter()
+            .fold(HashMap::new(), |mut acc, curr| {
+                let (atom, molecule) = curr;
+                acc.entry(atom)
+                    .or_insert_with(HashSet::new)
+                    .insert(molecule);
+                acc
+            });
+
+    println!("molecule = {}", molecule);
+    println!("replacement_map = {:?}", replacement_map);
 }
