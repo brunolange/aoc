4121d2852340febd4c6c744d88d66397c0fd6c50
Bruno Lange <blangeram@gmail.com>
2023-09-12 20:09:42 -0700

    Takes 0-2 coins into consideration

--- 2015/21/rust/rpg/src/main.rs
+++ 2015/21/rust/rpg/src/main.rs
@@ -14,11 +14,72 @@ struct Armor {
     armor: usize,
 }
 
+#[derive(Clone, Debug)]
+enum Ring {
+    Damage {
+        name: String,
+        cost: usize,
+        damage: usize,
+    },
+    Defense {
+        name: String,
+        cost: usize,
+        armor: usize,
+    },
+}
+
+impl Ring {
+    fn cost(&self) -> usize {
+        match self {
+            Ring::Damage {
+                name: _,
+                cost,
+                damage: _,
+            } => *cost,
+            Ring::Defense {
+                name: _,
+                cost,
+                armor: _,
+            } => *cost,
+        }
+    }
+
+    fn damage(&self) -> usize {
+        match self {
+            Ring::Damage {
+                name: _,
+                cost: _,
+                damage,
+            } => *damage,
+            _ => 0,
+        }
+    }
+
+    fn armor(&self) -> usize {
+        match self {
+            Ring::Defense {
+                name: _,
+                cost: _,
+                armor,
+            } => *armor,
+            _ => 0,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum PlayerRing {
+    NoRing,
+    OneRing(Ring),
+    TwoRings(Ring, Ring),
+}
+
 #[derive(Debug)]
 struct Player {
     hit_points: usize,
     weapon: Weapon,
     armor: Option<Armor>,
+    rings: PlayerRing,
 }
 
 struct Boss {
@@ -82,27 +143,66 @@ fn main() {
             armor: 5,
         },
     ];
+    let rings = vec![
+        Ring::Damage {
+            name: "Damage +1".to_owned(),
+            cost: 25,
+            damage: 1,
+        },
+        Ring::Damage {
+            name: "Damage +2".to_owned(),
+            cost: 50,
+            damage: 2,
+        },
+        Ring::Damage {
+            name: "Damage +3".to_owned(),
+            cost: 100,
+            damage: 3,
+        },
+        Ring::Defense {
+            name: "Defense +1".to_owned(),
+            cost: 20,
+            armor: 1,
+        },
+        Ring::Defense {
+            name: "Defense +2".to_owned(),
+            cost: 40,
+            armor: 2,
+        },
+        Ring::Defense {
+            name: "Defense +3".to_owned(),
+            cost: 80,
+            armor: 3,
+        },
+    ];
 
     let boss = Boss {
-        hit_points: 12,
-        damage: 7,
+        hit_points: 109,
+        damage: 8,
         armor: 2,
     };
 
-    let most_efficient_winning_player = iproduct!(weapons, armors)
+    let most_efficient_winning_player: Player = iproduct!(weapons.iter(), armors.iter())
         .flat_map(|(weapon, armor)| {
-            vec![
-                Player {
-                    hit_points: 100,
-                    weapon: weapon.clone(),
-                    armor: Some(armor),
-                },
-                Player {
-                    hit_points: 100,
-                    weapon: weapon,
-                    armor: None,
-                },
-            ]
+            let armor_combinations = vec![None, Some(armor.clone())];
+
+            let ring_combinations = vec![PlayerRing::NoRing]
+                .into_iter()
+                .chain(rings.iter().map(|r| PlayerRing::OneRing(r.clone())))
+                .chain(
+                    iproduct!(rings.iter().enumerate(), rings.iter().enumerate())
+                        .filter(|((i, _), (j, _))| i != j)
+                        .map(|((_, left), (_, right))| {
+                            PlayerRing::TwoRings(left.clone(), right.clone())
+                        }),
+                );
+
+            iproduct!(armor_combinations, ring_combinations).map(|(armor, rings)| Player {
+                hit_points: 100,
+                weapon: weapon.clone(),
+                armor,
+                rings,
+            })
         })
         .filter(|player| can_beat(player, &boss))
         .min_by_key(|player| {
@@ -111,8 +211,14 @@ fn main() {
             } else {
                 0_usize
             };
-            player.weapon.cost + armor_cost
-        });
+            let rings_cost = match &player.rings {
+                PlayerRing::NoRing => 0_usize,
+                PlayerRing::OneRing(r) => r.cost(),
+                PlayerRing::TwoRings(left, right) => left.cost() + right.cost(),
+            };
+            player.weapon.cost + armor_cost + rings_cost
+        })
+        .unwrap();
 
     println!(
         "most_efficient_winning_player = {:?}",
@@ -127,8 +233,26 @@ fn can_beat(player: &Player, boss: &Boss) -> bool {
         0_usize
     };
 
-    let player_deal = std::cmp::min(1, player.weapon.damage - boss.armor);
-    let boss_deal = std::cmp::min(1, boss.damage - player_armor);
+    let player_rings_damage = match &player.rings {
+        PlayerRing::OneRing(ring) => ring.damage(),
+        PlayerRing::TwoRings(left, right) => left.damage() + right.damage(),
+        _ => 0,
+    };
+
+    let player_rings_armor = match &player.rings {
+        PlayerRing::OneRing(ring) => ring.armor(),
+        PlayerRing::TwoRings(left, right) => left.armor() + right.armor(),
+        _ => 0,
+    };
+
+    let player_deal = std::cmp::max(
+        1,
+        player.weapon.damage as i64 + player_rings_damage as i64 - boss.armor as i64,
+    ) as usize;
+    let boss_deal = std::cmp::max(
+        1,
+        boss.damage as i64 - player_armor as i64 - player_rings_armor as i64,
+    ) as usize;
 
     let player_required_turns = boss.hit_points / player_deal;
     let boss_required_turns = player.hit_points / boss_deal;
