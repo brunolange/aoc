cbf91bb8a079998d8833dbb8c3536eeeb086ffb2
Bruno Lange <blangeram@gmail.com>
2023-10-15 18:59:47 -0700

    Naming things. Always good

--- 2015/25/rust/cop/src/main.rs
+++ 2015/25/rust/cop/src/main.rs
@@ -1,11 +1,15 @@
 use itertools::iterate;
 
+const INITIAL_VALUE: usize = 20_151_125;
+const MULTIPLIER: usize = 252_533;
+const MODULUS: usize = 33_554_393;
+
 fn code(row: usize, column: usize) -> usize {
     let max_row = row + column - 1;
     let steps_to_max_row = max_row * (max_row - 1) / 2; // max_row choose 2
     let steps = steps_to_max_row + column;
 
-    iterate(20_151_125, |curr| (curr * 252_533) % 33_554_393)
+    iterate(INITIAL_VALUE, |curr| (curr * MULTIPLIER) % MODULUS)
         .take(steps)
         .last()
         .unwrap()
