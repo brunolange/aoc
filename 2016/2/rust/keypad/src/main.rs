4a6c5a8b221dcecd8f171285681e72633fb547fc
Bruno Lange <blangeram@gmail.com>
2023-11-03 21:51:27 -0700

    Adds io module

--- 2016/2/rust/keypad/src/main.rs
+++ 2016/2/rust/keypad/src/main.rs
@@ -1,15 +1,5 @@
 use std::str::FromStr;
-use std::{fs::File, io::BufRead};
-
-pub fn lines() -> Box<dyn Iterator<Item = String>> {
-    match std::env::args().nth(1) {
-        None => Box::new(std::io::stdin().lock().lines().filter_map(Result::ok)),
-        Some(path) => {
-            let file = File::open(path).expect("Error reading file");
-            Box::new(std::io::BufReader::new(file).lines().filter_map(Result::ok))
-        }
-    }
-}
+mod io;
 
 #[derive(Debug)]
 enum Instruction {
@@ -137,5 +127,5 @@ fn decode(
 }
 
 fn main() {
-    decode(Button::Five, lines()).for_each(|button| println!("{:?}", button));
+    decode(Button::Five, io::lines()).for_each(|button| println!("{:?}", button));
 }
