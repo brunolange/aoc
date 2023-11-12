fb6158afc2e9cc7486b899acd774dd5363a4e836
Bruno Lange <blangeram@gmail.com>
2023-11-12 10:53:38 -0800

    Submits Rust solution for AoC 2016/4 part 2

--- 2016/4/rust/rooms/src/main.rs
+++ 2016/4/rust/rooms/src/main.rs
@@ -8,8 +8,33 @@ fn main() {
     let output = io::lines()
         .map(|line| line.parse::<Room>().unwrap())
         .filter(|room| room.is_real())
+        .inspect(|room| {
+            let msg = decode(&room.name, room.sector_id);
+            println!("{:?} => {}", room, msg);
+        })
         .map(|room| room.sector_id)
         .sum::<usize>();
 
     println!("output = {}", output);
+}
+
+const LETTERS: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+
+fn decode(s: &str, sector_id: usize) -> String {
+    assert!(sector_id != 0, "sector_id cannot be zero");
+    s.chars()
+        .map(|c| {
+            if c == '-' {
+                return ' ';
+            }
+            let offset = c as usize - 97;
+            let index = offset + sector_id;
+            LETTERS[index % 26]
+            // let step = offset + sector_id + 1;
+            // ('a'..='z').cycle().take(step).last().unwrap()
+        })
+        .collect::<String>()
 }
