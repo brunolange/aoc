801e74faa4f68ee0e9d7073d79c7195870c3f1ad
Bruno Lange <blangeram@gmail.com>
2023-12-05 17:28:44 -0800

    Applies map with a named function

--- 2016/6/rust/ecc/src/main.rs
+++ 2016/6/rust/ecc/src/main.rs
@@ -1,7 +1,17 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
 mod io;
 mod utils;
+
+fn to_counter<T: Hash + Eq>(vec: Vec<T>) -> HashMap<T, usize> {
+    let mut counter = HashMap::new();
+    vec.into_iter().for_each(|c| {
+        let count = counter.entry(c).or_insert(0 as usize);
+        *count += 1;
+    });
+    counter
+}
 
 fn main() {
     let code: String = utils::transpose(
@@ -10,14 +20,7 @@ fn main() {
             .collect(),
     )
     .into_iter()
-    .map(|column| {
-        let mut count_map = HashMap::new();
-        column.into_iter().for_each(|c| {
-            let count = count_map.entry(c).or_insert(0 as usize);
-            *count += 1;
-        });
-        count_map
-    })
+    .map(to_counter)
     .map(|count_map| {
         let (max_char, _) = count_map
             .into_iter()
