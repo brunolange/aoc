b8ca86f6f12ab3153f1381e9a576d28349d58132
Bruno Lange <blangeram@gmail.com>
2023-12-13 21:21:48 -0800

    Adds parser for Marker

--- 2016/9/rust/decomp/src/lib.rs
+++ 2016/9/rust/decomp/src/lib.rs
@@ -1,9 +1,18 @@
+use nom::{
+    bytes::complete::tag,
+    sequence::{delimited, separated_pair},
+    IResult,
+};
 use std::str::FromStr;
+
+mod parsers;
+
+use parsers::parse_usize;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Marker {
     pub take: usize,
-    pub repeate: usize,
+    pub repeat: usize,
 }
 
 #[derive(Debug)]
@@ -13,6 +22,34 @@ impl FromStr for Marker {
     type Err = MarkerParsingError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        let (_, marker) = parser_marker(s)
+            .map_err(|_| MarkerParsingError("detailed error goes here".to_string()))?;
+        Ok(marker)
+    }
+}
+
+fn parser_marker(s: &str) -> IResult<&str, Marker> {
+    let (s, (take, repeat)) = delimited(
+        tag("("),
+        separated_pair(parse_usize, tag("x"), parse_usize),
+        tag(")"),
+    )(s)?;
+
+    Ok((s, Marker { take, repeat }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_marker_parser() {
+        assert_eq!(
+            "(10x2)".parse::<Marker>().unwrap(),
+            Marker {
+                take: 10,
+                repeat: 2
+            }
+        )
     }
 }
--- 2016/9/rust/decomp/src/parsers.rs
+++ 2016/9/rust/decomp/src/parsers.rs
@@ -1,23 +1,5 @@
-use nom::IResult;
+use nom::{character::complete::digit1, combinator::map_res, IResult};
 
-use decomp::Marker;
-
-pub fn parser_marker(s: &str) -> IResult<&str, Marker> {
-    todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_marker_parser() {
-        assert_eq!(
-            "(10x2)".parse::<Marker>().unwrap(),
-            Marker {
-                take: 10,
-                repeate: 2
-            }
-        )
-    }
+pub fn parse_usize(s: &str) -> IResult<&str, usize> {
+    map_res(digit1, str::parse)(s)
 }
