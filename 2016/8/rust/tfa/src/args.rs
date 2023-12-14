7e1dc99e3b9ed50867208a3dce06e3f67fb77c4d
Bruno Lange <blangeram@gmail.com>
2023-12-13 18:29:21 -0800

    Adds interactive mode

--- 2016/8/rust/tfa/src/args.rs
+++ 2016/8/rust/tfa/src/args.rs
@@ -3,7 +3,11 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct MyArgs {
-    /// interval in milliseconds to pause between animation frames
+    /// enable to show animation
+    #[clap(long, default_value_t = false)]
+    pub interactive: bool,
+
+    /// interval in milliseconds to pause between animation frames. Only relevant with --interactive
     #[clap(long, default_value_t = 10)]
     pub pause: u64,
 }
--- 2016/8/rust/tfa/src/main.rs
+++ 2016/8/rust/tfa/src/main.rs
@@ -9,14 +9,18 @@ fn main() {
 
     let mut grid = Grid([[false; 50]; 6]);
 
-    io::clear();
-    io::flash(&format!("{grid}"), args.pause);
+    if args.interactive {
+        io::clear();
+        io::flash(&format!("{grid}"), args.pause);
+    }
 
     io::lines()
         .map(|line| line.parse().expect("invalid instruction"))
         .for_each(|instruction| {
             grid.apply(&instruction);
-            io::flash(&format!("{grid}"), args.pause);
+            if args.interactive {
+                io::flash(&format!("{grid}"), args.pause);
+            }
         });
 
     println!("{grid}");
