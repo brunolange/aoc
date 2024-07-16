73ef568f4b58528223708cc9afb3219be22cc8a1
Bruno Lange <blangeram@gmail.com>
2024-07-15 17:09:40 -0700

    Disagreeing with the formatter to make iter vs par_iter more obvious

--- 2015/4/rust/adventcoins/src/lib.rs
+++ 2015/4/rust/adventcoins/src/lib.rs
@@ -2,17 +2,21 @@ use md5;
 use rayon::prelude::*;
 
 pub fn nonce(msg: &str, prefix: &str) -> Option<u64> {
-    (1u64..u64::MAX).into_iter().find(|&i| {
-        let word = std::format!("{}{}", msg, i);
-        let digest = md5::compute(word);
-        std::format!("{:x}", digest).starts_with(prefix)
-    })
+    (1u64..u64::MAX)
+        .into_iter()
+        .find(|&i| {
+            let word = std::format!("{}{}", msg, i);
+            let digest = md5::compute(word);
+            std::format!("{:x}", digest).starts_with(prefix)
+        })
 }
 
 pub fn nonce_par(msg: &str, prefix: &str) -> Option<u64> {
-    (1u64..u64::MAX).into_par_iter().find_first(|&i| {
-        let word = std::format!("{}{}", msg, i);
-        let digest = md5::compute(word);
-        std::format!("{:x}", digest).starts_with(prefix)
-    })
+    (1u64..u64::MAX)
+        .into_par_iter()
+        .find_first(|&i| {
+            let word = std::format!("{}{}", msg, i);
+            let digest = md5::compute(word);
+            std::format!("{:x}", digest).starts_with(prefix)
+        })
 }
